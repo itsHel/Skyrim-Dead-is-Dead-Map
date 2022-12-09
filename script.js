6f8d0972d5ab81781a83e6b794bb221deee0bfd9
itsHel <51739304+itsHel@users.noreply.github.com>
2022-12-09 11:55:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -46,8 +46,8 @@
     let url = ajaxUrl + "load-data.php";
     fetch(ajaxUrl)
         .then(response => response.text())
-        .then(text => console.log(text));
-        .catch(err) => {console.log(err)}
+        .then(text => console.log(text))
+        .catch((err) => {console.log(err)});
 
     if(map.complete){
         mapWrapper.style.opacity = 1;

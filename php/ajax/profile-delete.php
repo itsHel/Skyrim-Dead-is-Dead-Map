1783c651a18b4adfbc9fa29a365ab79851d1c8a0
itsHel <51739304+itsHel@users.noreply.github.com>
2022-12-10 02:40:59 +0100

    Update profile-delete.php

--- php/ajax/profile-delete.php
+++ php/ajax/profile-delete.php
@@ -5,6 +5,11 @@
     OR db error
 */  
 
+    header('Access-Control-Allow-Origin: https://itshel.github.io');
+    header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
+
     session_start();
 
     if(!($_SESSION["user"] ?? ""))

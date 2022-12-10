5257829b4bbe0b650d76c22fdcf2f773c28efeeb
itsHel <51739304+itsHel@users.noreply.github.com>
2022-12-10 02:40:23 +0100

    Update forgot-create.php

--- php/ajax/forgot-create.php
+++ php/ajax/forgot-create.php
@@ -7,6 +7,11 @@
     OR db error
 */
 
+    header('Access-Control-Allow-Origin: https://itshel.github.io');
+    header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
+
     require "../mail.php";
     require "../main.php";
     require "../database.php";

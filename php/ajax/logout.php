1502494a84ddcdda81a32425421c0a5b548a97d4
itsHel <51739304+itsHel@users.noreply.github.com>
2022-12-10 02:41:07 +0100

    Update logout.php

--- php/ajax/logout.php
+++ php/ajax/logout.php
@@ -4,6 +4,11 @@
         "logged-out"
 */     
 
+    header('Access-Control-Allow-Origin: https://itshel.github.io');
+    header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
+
     session_start();
     $_SESSION = [];
     session_destroy();
